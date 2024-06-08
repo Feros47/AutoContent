3b6c54fe2fce3b29c552ce00ba3b7df0ce74c3b0
Feros47 <Niklasm47@live.dk>
2024-06-08 19:03:02 +0200

    whatevrer

--- AutoContent/obj/Debug/net8.0/AutoContent.AssemblyInfo.cs
+++ AutoContent/obj/Debug/net8.0/AutoContent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoContent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df904be64eba6c07f3f1f788f28329cb9ec471d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c478e60ab59c6e1503ee3ab6d3921fca789a15a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoContent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoContent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
